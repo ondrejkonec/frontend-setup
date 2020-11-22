69f72e8452b190f725c3377d8bc9ae631fefc319
Ondřej Konečný <ondrej.konecny@gmail.com>
2020-11-22 17:20:52 +0100

    renaming

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,8 @@
 module.exports = function (grunt) {
+
   // Configure main project settings
   grunt.initConfig({
+    
     // Basic settings and info about our plugins
     pkg: grunt.file.readJSON("package.json"),
 
